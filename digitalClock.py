340bfd96c64e461756d28ae9b675aa48da07627b
techtfq <techtfq@gmail.com>
2023-04-19 10:06:50 -0400

    removed welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -3,9 +3,9 @@ from tkinter import Tk, Label
 window =Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="blue")
 
-label = Label(window, text= "Welcome!", font=("Arial Black",78,"bold"),bg ="steelblue", fg = "white")
+label = Label(window, font=("Arial Black",78,"bold"),bg ="blue", fg = "white")
 label.pack(pady=100)
 
 window.mainloop()
